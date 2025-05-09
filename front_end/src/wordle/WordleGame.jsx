886d134bd355d54e93c6fde470d2df0e9467ff8a
Jason Belt <jasondbelt@gmail.com>
2025-05-09 11:26:04 -0500

    i think it works

--- front_end/src/wordle/WordleGame.jsx
+++ front_end/src/wordle/WordleGame.jsx
@@ -9,7 +9,6 @@ import { WordleContext } from './WordleContext';
 const WordleGame = () => {
   const [board, setBoard] = useState(boardDefault)
   const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0})
-  const [wordSet, setWordSet] = useState(new Set())
   const [disabledLetters, setDisabledLetters] = useState([])
   const [correctWord, setCorrectWord] = useState("")
   const [gameOver, setGameOver] = useState({
@@ -17,15 +16,11 @@ const WordleGame = () => {
     guessedWord: false,
   })
 
-  // const correctWord = "RIGHT"
-
   useEffect(() => {
     generateWordSet().then((words) => {
-      setWordSet(words.wordSet)
       setCorrectWord(words.todaysWord.toUpperCase())
     })
   }, [])
-
 
   const onSelectLetter = (keyVal) => {
     if (currAttempt.letterPos > 4) return;
@@ -38,55 +33,50 @@ const WordleGame = () => {
   const onDelete = () => {
     if (currAttempt.letterPos === 0) return;
     const newBoard = [...board]
-    newBoard[currAttempt.attempt][currAttempt.letterPos -1] = "";
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
     setBoard(newBoard)
     setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos - 1})
   }
 
   const onEnter = () => {
     if (currAttempt.letterPos !== 5) return;
-  
+
     let currWord = "";
     for (let i = 0; i < 5; i++) {
       currWord += board[currAttempt.attempt][i];
     }
-  
+
     if (currWord === correctWord) {
       setGameOver({ gameOver: true, guessedWord: true });
     } else if (currAttempt.attempt === 5) {
       setGameOver({ gameOver: true, guessedWord: false });
     }
-  
+
     setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
   };
-  
 
   const handleReset = () => {
     window.location.reload();
   }
-  
+
   return (
-      <div className="wordleGame">
-        <WordleContext.Provider 
-          value={{
-            board, setBoard, 
-            currAttempt, setCurrAttempt, 
-            onSelectLetter, onDelete, onEnter, 
-            correctWord, 
-            disabledLetters, setDisabledLetters, 
-            gameOver, setGameOver}}>
-          <div className="wordleGame">
-            <Board />
-            <button onClick={handleReset} className="reset-button">Reset</button>
-            {gameOver.gameOver ? <GameOver/> : <KeyBoard />}
-          </div>
-        </WordleContext.Provider>
-      </div>
+    <div className="wordleGame">
+      <WordleContext.Provider 
+        value={{
+          board, setBoard, 
+          currAttempt, setCurrAttempt, 
+          onSelectLetter, onDelete, onEnter, 
+          correctWord, 
+          disabledLetters, setDisabledLetters, 
+          gameOver, setGameOver}}>
+        <div className="wordleGame">
+          <Board />
+          <button onClick={handleReset} className="reset-button">Reset</button>
+          {gameOver.gameOver ? <GameOver /> : <KeyBoard />}
+        </div>
+      </WordleContext.Provider>
+    </div>
   );
 };
 
 export default WordleGame;
-
-
-
-
