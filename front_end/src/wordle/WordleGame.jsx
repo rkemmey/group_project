071347665afecf55f738c3fcdf18fa664cc301cd
Jason Belt <jasondbelt@gmail.com>
2025-05-09 11:01:10 -0500

    last bug

--- front_end/src/wordle/WordleGame.jsx
+++ front_end/src/wordle/WordleGame.jsx
@@ -11,6 +11,7 @@ const WordleGame = () => {
   const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0})
   const [wordSet, setWordSet] = useState(new Set())
   const [disabledLetters, setDisabledLetters] = useState([])
+  // const [correctWord, setCorrectWord] = useState("")
   const [gameOver, setGameOver] = useState({
     gameOver: false,
     guessedWord: false,
@@ -21,6 +22,7 @@ const WordleGame = () => {
   useEffect(() => {
     generateWordSet().then((words) => {
       setWordSet(words.wordSet)
+      // setCorrectWord(words.todaysWord)
     })
   }, [])
 
