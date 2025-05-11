f004c6c077de0812bf43836b7e072766af568358
Jason Belt <jasondbelt@gmail.com>
2025-05-11 12:51:15 -0500

    backend might be good

--- back_end/minewordle_app/models.py
+++ back_end/minewordle_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Word(models.Model):
@@ -7,3 +8,20 @@ class Word(models.Model):
     class Meta:
         db_table = 'words'
 
+
+class Score(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    difficulty = models.CharField(max_length=10, choices=[
+        ('easy', 'Easy'),
+        ('medium', 'Medium'),
+        ('hard', 'Hard'),
+    ])
+    score = models.PositiveIntegerField()
+
+    # highest scores first
+    class Meta:
+        db_table = 'scores'
+        ordering = ['-score']  
+
+    def __str__(self):
+        return f"{self.user.username} - {self.score} ({self.difficulty})"
--- back_end/minewordle_app/views.py
+++ back_end/minewordle_app/views.py
@@ -2,14 +2,44 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.db.models.functions import Random
 from .models import Word
+from rest_framework import generics, permissions
+from rest_framework.authentication import TokenAuthentication
+from .models import Score
+from .serializers import ScoreSerializer
+
 
 # Create your views here.
 def get_random_word(request):
     if request.method == 'GET':
-        word = Word.objects.order_by(Random()).first()  # Querying the 'words' table
+         # Querying the 'words' table
+        word = Word.objects.order_by(Random()).first()
         if word:
             return JsonResponse({'word': word.word})
         return JsonResponse({'error': 'No words in database'}, status=404)
+    
+
+"""
+1. login post request: http://127.0.0.1:8000/api/user/login/
+2. get scores request: http://127.0.0.1:8000/api/minewordle/user_top_scores/
+   headers: Key: Authorization
+            Value: Token 7d96e3ecb787c4c429b4b9b6462118ff5b55ef00
+3. post scores request: same as get, but ensure you use post instead of get
+   example data after logging in:
+   {
+    "difficulty": "medium",
+    "score": 100
+   }
+"""
+class UserTopScoresView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ScoreSerializer
+
+    def get_queryset(self):
+        return Score.objects.filter(user=self.request.user).order_by('-score')[:10]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 
